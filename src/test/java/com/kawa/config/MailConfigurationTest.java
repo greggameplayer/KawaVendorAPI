34a6227b41928b866dc935b80d64904d9ad5da9d
greggameplayer <gregoire.hage@gmail.com>
2023-03-13 23:18:27 +0100

    add AutoConfigureMockMvc annotation on MailConfigurationTest

--- src/test/java/com/kawa/config/MailConfigurationTest.java
+++ src/test/java/com/kawa/config/MailConfigurationTest.java
@@ -15,9 +15,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.mail.javamail.JavaMailSender;
 
 @IntegrationTest
+@AutoConfigureMockMvc
 public class MailConfigurationTest {
 
     private static final String DEFAULT_EMAIL = "AAAAAAAAAA@gmail.com";
