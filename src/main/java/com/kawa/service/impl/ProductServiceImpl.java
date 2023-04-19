98ef1b690b6f8bb3e47590c15ce157caf6ac4801
greggameplayer <gregoire.hage@gmail.com>
2023-04-19 15:00:41 +0200

    delete restTemplate for now

--- src/main/java/com/kawa/service/impl/ProductServiceImpl.java
+++ src/main/java/com/kawa/service/impl/ProductServiceImpl.java
@@ -3,7 +3,6 @@ package com.kawa.service.impl;
 import com.kawa.domain.bean.Product;
 import com.kawa.service.ProductService;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Service Implementation for managing {@link Product}.
@@ -13,12 +12,6 @@ import org.springframework.web.client.RestTemplate;
 public class ProductServiceImpl implements ProductService {
 
     private static final String API_KEY = "IcpnqyikSl3vtUgwOMlodLyosYYkmQSrcOzZMpuyExEsQlZRJAsX2HSDXqou4EbK";
-
-    private final RestTemplate restTemplate;
-
-    public ProductServiceImpl(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 
     @Override
     public Product getProduct() {
